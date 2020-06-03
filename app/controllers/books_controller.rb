2b969b16ed458bd8654d611259d30cb2870f7ff3
vagrant <vagrant@localhost.localdomain>
2020-06-03 06:35:52 +0000

    edit add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,11 @@
 class BooksController < ApplicationController
   def index
+  	  #記事を全件取得
+  	  @books = Book.all
   end
 
   def show
+  	  @book = Book.find(params[:id]) 
   end
 
   def new
@@ -12,9 +15,23 @@ class BooksController < ApplicationController
   def create
   	  book = Book.new(book_params)
   	  book.save
-      redirect_to bool_path
+      redirect_to book_path(book.id)
   end
 
   def edit
+  	  @book = Book.find(params[:id])
+  end
+
+  def update
+  	  book = Book.find(paramd[:id])
+  	  book.update(book_params)
+  	  redirect_to book_path(book)
+  end
+
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
+
