c8348cc825e7d970e583ec04ecf6cbd3d23e6708
vagrant <vagrant@localhost.localdomain>
2020-06-03 06:11:33 +0000

    form add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,13 @@ class BooksController < ApplicationController
   end
 
   def new
+  	  @book = Book.new
+  end
+
+  def create
+  	  book = Book.new(book_params)
+  	  book.save
+      redirect_to bool_path
   end
 
   def edit
